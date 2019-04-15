1ee0706731e058db82e6eef6b52a1f8a85f1aa43
nckearly <47195109+nckearly@users.noreply.github.com>
2019-04-15 10:59:54 -0600

    Add files via upload

---
+++ docs/precache-manifest.c37b3c8722048a010c495c5b5967ac44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8e1a1556280c7458213",
+    "url": "/dev/src/sigtaucolostate/build/static/js/runtime~main.c8e1a155.js"
+  },
+  {
+    "revision": "b74794136f5b50b7f6a6",
+    "url": "/dev/src/sigtaucolostate/build/static/js/main.b7479413.chunk.js"
+  },
+  {
+    "revision": "3c5be2411b0f2fb5524a",
+    "url": "/dev/src/sigtaucolostate/build/static/js/2.3c5be241.chunk.js"
+  },
+  {
+    "revision": "b74794136f5b50b7f6a6",
+    "url": "/dev/src/sigtaucolostate/build/static/css/main.535a437c.chunk.css"
+  },
+  {
+    "revision": "8df5a3b97c23f8bf7fe965f4e34d4a10",
+    "url": "/dev/src/sigtaucolostate/build/index.html"
+  }
+];

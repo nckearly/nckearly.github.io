fb405df5974224b9bc2c9f73471c58b520b664b7
nckearly <47195109+nckearly@users.noreply.github.com>
2019-04-15 11:17:41 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 /* eslint-disable react/prefer-stateless-function */
-import React from 'react';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import theme from './assets/theme';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import theme from './assets/theme';
+import Alumni from './pages/Alumni';
+import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Join from './pages/Join';
-import Contact from './pages/Contact';
-import Outreach from './pages/Outreach';
-import Alumni from './pages/Alumni';
-import WhiteRose from './pages/White-Rose';
-import Members from './pages/Members';
 import Loader from './pages/Loader';
+import Members from './pages/Members';
+import Outreach from './pages/Outreach';
+import WhiteRose from './pages/White-Rose';
 
 class App extends React.Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <Router>
+        <Router basename="/sigtaucolostate">
           <Switch>
-          <Route component={Home} path="/" exact />
+            <Route component={Home} path="/" exact />
             <Route component={Join} path="/join/" />
             <Route component={Contact} path="/Contact/" />
             <Route component={Outreach} path="/Outreach/" />
@@ -26,7 +26,7 @@ class App extends React.Component {
             <Route component={WhiteRose} path="/White-Rose/" />
             <Route component={Members} path="/Members/" />
             <Route component={Loader} path="/Loader/" />
-            <Route component={() => "Error: 404"} />
+            <Route component={() => 'Error: 404'} />
           </Switch>
         </Router>
       </MuiThemeProvider>
@@ -34,4 +34,4 @@ class App extends React.Component {
   }
 }
 
-export default (App);
+export default App;

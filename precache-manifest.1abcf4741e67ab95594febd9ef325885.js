79bc175410c7488d6af4c724af36940341aab017
nckearly <47195109+nckearly@users.noreply.github.com>
2020-01-26 19:52:40 -0700

    Updates

---
+++ precache-manifest.1abcf4741e67ab95594febd9ef325885.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5414383d92e7d1ca7e1e",
+    "url": "/static/css/main.011b3993.chunk.css"
+  },
+  {
+    "revision": "5414383d92e7d1ca7e1e",
+    "url": "/static/js/main.5414383d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "9b7c4dc07c62430238a3",
+    "url": "/static/js/2.9b7c4dc0.chunk.js"
+  },
+  {
+    "revision": "b00f0000fb2ed78ceee89fe3f4bc681f",
+    "url": "/index.html"
+  }
+];
